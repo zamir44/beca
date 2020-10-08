2be40464ff2b1dcb92f029760149e9e8e60b3e61
zamir44 <tooradicalxxi@gmail.com>
2020-10-08 22:22:38 +0200

    can't fix the animation on service.html

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-
-// var controller = new ScrollMagic.Controller();
-
-// var scene = new ScrollMagic.Scene({
-//     triggerElement: '.right__circle'
-// })
-
-// .setClassToggle('.right__circle', 'show')
-// .addTo(controller);
